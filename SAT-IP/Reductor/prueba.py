872bd91d53b8a9fc6ea6820e4d2bf7f005e9c003
juanJvargas <you@example.com>
2019-07-25 12:28:51 -0500

    Arreglo caracteres % 0 y lineas vacias

--- SAT-IP/Reductor/prueba.py
+++ SAT-IP/Reductor/prueba.py
@@ -9,12 +9,14 @@ def leer():
         for f in onlyfiles:
             rutaL="../InstanciasSAT/"+f#Ruta a los archivos de lectura
             name= f.split(".")
+            #print(name)
             rutaE="../InstanciasMiniZinc/reduccion_"+name[0]+".mzn"#Ruta a los archivos de escritura
             archivoR = open(rutaL, "r") 
             lineas = archivoR.readlines()
             result = {'variables': 0, 'constraints': 0, 'lines':[]}
             for linea in lineas:
-                if linea[0] != 'c' and linea[0] != 'p': result['lines'].append(linea) #Aade a lines las lineas que corresponden a clausulas
+                #linea.replace("%","")
+                if linea[0] != 'c' and linea[0] != 'p' and linea[0] != '%' and linea[0] != '0' and linea[0] != '\n': result['lines'].append(linea) #Aade a lines las lineas que corresponden a clausulas
                 if linea[0] == 'p':
                     lineaP = linea.split()
                     result['variables'] = int(lineaP[2])
@@ -26,33 +28,38 @@ def leer():
         return None
 
 def convertir(ruta, datos):
-    archivoW = open(ruta, "w")
-    #Construir definicion de variables
-    for x in range(1, datos['variables']+1):
-        archivoW.write("var 0..1: v"+ str(x) + "; var 0..1: n_v" + str(x) +";\n")
-        archivoW.write("constraint v"+ str(x) + " + n_v" + str(x) +" = 1;\n\n")
+    try: 
+        archivoW = open(ruta, "w")
+        #Construir definicion de variables
+        for x in range(1, datos['variables']+1):
+            archivoW.write("var 0..1: v"+ str(x) + "; var 0..1: n_v" + str(x) +";\n")
+            archivoW.write("constraint v"+ str(x) + " + n_v" + str(x) +" = 1;\n\n")
 
-    for linea in datos['lines']:
-        elementos = linea.split()
-        elementos.pop()
-        archivoW.write("constraint ")
-        iterator = 0
-        if elementos[iterator][0] == '-':
-            elementos[iterator] = elementos[iterator].replace("-","")
-            archivoW.write("n_v"+elementos[iterator])
-        else:
-            archivoW.write("v"+elementos[iterator])
-        iterator += 1
-        while iterator < len(elementos):
-            archivoW.write(" + ")
-            if elementos[iterator][0] == "-":
+        for linea in datos['lines']:
+            elementos = linea.split()
+            #print(elementos)
+            elementos.pop()
+            archivoW.write("constraint ")
+            iterator = 0
+            if elementos[iterator][0] == '-':
                 elementos[iterator] = elementos[iterator].replace("-","")
                 archivoW.write("n_v"+elementos[iterator])
             else:
                 archivoW.write("v"+elementos[iterator])
             iterator += 1
-        archivoW.write(" >= 1;\n")
-    archivoW.write("solve satisfy;")
+            while iterator < len(elementos):
+                archivoW.write(" + ")
+                if elementos[iterator][0] == "-":
+                    elementos[iterator] = elementos[iterator].replace("-","")
+                    archivoW.write("n_v"+elementos[iterator])
+                else:
+                    archivoW.write("v"+elementos[iterator])
+                iterator += 1
+            archivoW.write(" >= 1;\n")
+        archivoW.write("solve satisfy;")
+    except Exception as e:
+        print("Error[convertir]: ", e)
+        return None
 
 def main (entrada):
     try:
